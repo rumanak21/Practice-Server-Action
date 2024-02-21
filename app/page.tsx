507b198b5858e4e179958fbc58457da356406e3f
rumanak21 <rumana2k20@gmail.com>
2024-02-21 13:46:03 +0100

    Created a function to get data

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,49 @@
 import Image from "next/image";
+import prisma from './db'
+import { revalidatePath } from "next/cache";
 
-export default function Home() {
+async function getData() {
+  const data = await prisma.todo.findMany({
+    select: {
+      input: true,
+      id: true
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+
+  return data;
+}
+
+
+
+export default async function Home() {
+  const data = await getData()
+  async function create(formData: FormData) {
+    "use server"
+    const input = formData.get('input') as string;
+    await prisma.todo.create({
+      data: {
+        input: input
+      }
+    })
+    revalidatePath('/')
+  }
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       <div className="border rounded-lg shadow-xl p-10 w-[70vw]">
-        <form className="flex flex-col">
+        <form action={create} className="flex flex-col">
           <input type="text" name="input" className="border p-1 border-gray-800" />
           <button className="bg-green-500 rounded-lg mt-2 text-white py-2" type="submit">Submit</button>
         </form>
+        <div className="mt-5 flex flex-col gap-y-2">
+          {
+            data.map((todo) => (
+              <p key={todo.id}>{todo.input}</p>
+            ))
+          }
+        </div>
       </div>
     </div>
   );
